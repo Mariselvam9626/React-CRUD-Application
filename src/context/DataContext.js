f5a54cfd50708f4ef8ba48ff03e5f80e78bb5342
Mariselvam <167742103+Mariselvam9626@users.noreply.github.com>
2024-08-02 12:56:31 +0530

    Update DataContext.js

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -24,30 +24,12 @@ export const DataProvider = ({children})=> {
       setPosts(data);
     },[data])
   
-  //   useEffect(() =>{
-  //     const fetchPosts = async () =>{
-  //     try {
-  //       const response = await api.get('/posts');
-  //       setPosts(response.data);
-  //     } catch (err) {
-  //       if (err.response) {
-  //         console.log(err.response.data)
-  //         console.log(err.response.status)
-  //         console.log(err.response.headers)
-  //       } else {
-  //         console.log(`Error: ${err.message}`);
-  //       }
-  //     }
-  //   }
-  //   fetchPosts();
-  // },[])
-  
-      useEffect (() => {
-          const filteredResults = posts. filter ((post) =>
-          ((post.body).toLowerCase()).includes (search.
-          toLowerCase())
-          || ((post.title).toLowerCase()).includes (search.
-          toLowerCase()));
+    useEffect (() => {
+        const filteredResults = posts. filter ((post) =>
+        ((post.body).toLowerCase()).includes (search.
+        toLowerCase())
+        || ((post.title).toLowerCase()).includes (search.
+        toLowerCase()));
       setSearchResults (filteredResults. reverse());
     }, [posts, search])
     
