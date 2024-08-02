a6f00a62e7624756425b24e573e5aaa25724510b
Mariselvam <mariselvam1999use@gmail.com>
2024-08-02 12:37:26 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,14 @@
+
+const About = () => {
+      return (
+        <main className='About'>
+            <h2>About</h2>
+            
+            <p style={{ marginTop: "1rem" }}>Hi, I am
+            Mariselvam. I try to become a Software Developer... 
+            </p>
+
+        </main>
+      )
+}
+export default About
--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes,Route,} from "react-router-dom";
+import About from "./About";
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+import Missing from "./Missing";
+import Nav from "./Nav";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import EditPost from "./EditPost";
+import { DataProvider } from "./context/DataContext";
 
 function App() {
-  return (
+
+  
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DataProvider>
+      {/* <nav>
+        <ul>
+          <li><Link to ="/">{<Home />}</Link></li>
+          <li><Link to ="/about">{<About />}</Link></li>
+          <li><Link to ="/postpage">{<PostPage />}</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+       
+       {/* it is called nested route */}
+{/*        
+        <Route path='/postpage' element={<PostLayout />}>    
+          <Route index element={<PostPage />} />
+          <Route path='newpost' element={<NewPost />} />
+          <Route path=":id" element={<Post />} />
+          <Route path="newpage" element={<Post />} />
+        </Route>
+        <Route path='*' element={<Missing />} />
+        
+      </Routes> */} 
+      
+        <Header title="CRUD APPLICATION" />
+        <Nav  />
+        <Routes>
+            <Route path="/" element ={
+              <Home />} />
+      
+            <Route path="post"> 
+              <Route index element ={<NewPost />} />
+              <Route path=":id" element={<PostPage />}            
+            /> 
+            </Route>
+            <Route path ="/edit/:id" element = {<EditPost />}
+            />
+        
+            <Route path="about" element={<About />} />
+            <Route path="*" element={<Missing />} />
+        </Routes>
+        <Footer /> 
+      </DataProvider>
+
     </div>
   );
 }
